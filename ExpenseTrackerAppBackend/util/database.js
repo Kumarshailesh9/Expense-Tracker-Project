a5e9012b5c0b3c34d52ce7f444efd212ad1aa5f6
Kumarshailesh9 <theshaileshkumar9@gmail.com>
2024-01-13 00:39:49 +0530

    adding cloud RDS connection

--- ExpenseTrackerAppBackend/util/database.js
+++ ExpenseTrackerAppBackend/util/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize( process.env.DB_SCHEMA, process.env.DB_USER , process.env.DB_PASSWORD , {
     dialect:  'mysql',
-    host:  'localhost'
+    host:  process.env.HOST_NAME
 });
 
 module.exports = sequelize;
